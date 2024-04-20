590e8e1841b9586c8d0785ae9c1cbbe7fb58dfd4
Arup-Dash02 <158983464+Arup-Dash02@users.noreply.github.com>
2024-04-20 17:23:26 +0530

    Learned about state management using recoil
    Learned what are the problems in context API,and learned how state management happened, how recoil works. Various hooks that recoil provides to manage the state

---
+++ practice 7.2/src/App.jsx
@@ -0,0 +1,48 @@
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "./store/atoms/count";
+
+function App() {
+  return (
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
+  );
+}
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Button />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return <div>{count}</div>;
+}
+
+function Button() {
+  const [count, setCount] = useRecoilState(countAtom);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+export default App;
