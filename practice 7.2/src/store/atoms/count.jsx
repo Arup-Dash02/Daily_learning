9c7271d0fb2194b3fdf183f60e83aea0d70da899
Arup-Dash02 <158983464+Arup-Dash02@users.noreply.github.com>
2024-04-21 01:25:39 +0530

    Update count.jsx

--- practice 7.2/src/store/atoms/count.jsx
+++ practice 7.2/src/store/atoms/count.jsx
@@ -1,6 +1,20 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const countAtom = atom({
   key: "countAtom",
   default: 0,
+});
+
+export const evenSelector = selector({
+  key: "evenSelector",
+  // get: (props) => {
+  //   const count = props.get(countAtom);
+  //   return count % 2;
+  // },
+  //the above code is exactly same as the below code.
+  //But below code is more recommended
+  get: ({ get }) => {
+    const count = get(countAtom);
+    return count % 2;
+  },
 });
